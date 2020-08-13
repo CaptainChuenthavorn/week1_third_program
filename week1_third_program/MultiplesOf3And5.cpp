48cc61892ef67158a289ddcc6df50b96a0084042
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-08-13 14:10:09 +0700

    add scanf to enter number from user

--- week1_third_program/MultiplesOf3And5.cpp
+++ week1_third_program/MultiplesOf3And5.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
 int main()
 {
-	int n, i, k, total=0;
-	for (i = 0;i < 1000;i++) {
+	int n, i, total=0;
+	printf("Enter number to calculate multiples of 3 and 5 sum : ");
+		scanf_s("%d", &n);
+	for (i = 0;i < n;i++) {
 		if (i % 3 == 0 || i % 5 == 0)
 		{
-			//printf("\n%d %d + %d ", total + i, total, i);
 			total += i;
 		}
 	}
-	printf("total : %d", total);
+	printf("total is  %d", total);
 	return 0;
 }
